df8f30010cc4cef46dcabc90a07e9f36d0eaa266
bala-amypo <bala@amypo.in>
2025-12-17 09:37:16 +0000

    User commit

--- repository/LocationRepository.java
+++ repository/LocationRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository
-public interface LocationRepository extends JpaRepository<LocationEntity,Long>{
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Jp;
+public interface LocationRepository extends JpaRepoaitory<LocationEntity,Object {
 
-    
-} 
+}
--- service/LocationServiceImpl.java
+++ service/LocationServiceImpl.java
@@ -16,21 +16,3 @@ public class LocationServiceImp implements LocationService {
     }
 
 }
-package com.example.demo.service;
-
-import com.example.demo.entity.LocationEntity;
-import com.example.demo.repository.LocationRepository;
-import com.example.demo.service.LocationService;
-
-public class LocationServiceImp implements LocationService {
-    @Autowired
-    LocationRepository Locationrepo;
-    @Override
-    public LocationEntity createlocation(LocationEntity le){
-        return Locationrepo.save(le);
-      public List<LocationEntity> getall(){
-        return Locationrepo.findAll();
-      }
-    }
-
-}
