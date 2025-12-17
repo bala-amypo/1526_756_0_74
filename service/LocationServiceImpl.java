60d22bc0f6f221f614cb7c2fb45f33d73b681e56
bala-amypo <bala@amypo.in>
2025-12-17 08:47:16 +0000

    User commit

---
+++ service/LocationServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.LocationEntity;
+import com.example.demo.repository.LocationRepository;
+import com.example.demo.service.LocationService;
+
+public class LocationServiceImp implements LocationService {
+    @Autowired
+    LocationRepository Locationrepo;
+    @Override
+    public LocationEntity createlocation(LocationEntity le){
+        return Locationrepo.save(le);
+      public List<LocationEntity> getall(){
+        return Locationrepo.findAll();
+      }
+    }
+
+}
+package com.example.demo.service;
+
+import com.example.demo.entity.LocationEntity;
+import com.example.demo.repository.LocationRepository;
+import com.example.demo.service.LocationService;
+
+public class LocationServiceImp implements LocationService {
+    @Autowired
+    LocationRepository Locationrepo;
+    @Override
+    public LocationEntity createlocation(LocationEntity le){
+        return Locationrepo.save(le);
+      public List<LocationEntity> getall(){
+        return Locationrepo.findAll();
+      }
+    }
+
+}
